c8f398c5fff6f14621f60328f80d0d317b5a38ea
Bitrise Dev <dev@bitrise.io>
2026-05-05 14:52:29 +0000

    Replace all UI sentences with 🐌

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,21 +11,12 @@ import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
 function getDevMenuHint() {
   if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
+    return <ThemedText type="small">🐌</ThemedText>;
   }
   if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
+    return <ThemedText type="small">🐌</ThemedText>;
   }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
+  return <ThemedText type="small">🐌</ThemedText>;
 }
 
 export default function HomeScreen() {
@@ -35,23 +26,23 @@ export default function HomeScreen() {
         <ThemedView style={styles.heroSection}>
           <AnimatedIcon />
           <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
+            🐌
           </ThemedText>
         </ThemedView>
 
         <ThemedText type="code" style={styles.code}>
-          get started
+          🐌
         </ThemedText>
 
         <ThemedView type="backgroundElement" style={styles.stepContainer}>
           <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
+            title="🐌"
+            hint={<ThemedText type="code">🐌</ThemedText>}
           />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
+          <HintRow title="🐌" hint={getDevMenuHint()} />
           <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
+            title="🐌"
+            hint={<ThemedText type="code">🐌</ThemedText>}
           />
         </ThemedView>
 
